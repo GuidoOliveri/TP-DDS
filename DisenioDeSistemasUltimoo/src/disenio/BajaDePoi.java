6256b0462693b7b2347a679f004d1e47b8be6242
Ignacio Araldi <iaraldi@hotmail.com>
2016-08-25 18:13:07 -0300

    agrego funcionalidad BajaDePoi

--- DisenioDeSistemasUltimoo/src/disenio/BajaDePoi.java
+++ DisenioDeSistemasUltimoo/src/disenio/BajaDePoi.java
@@ -1,11 +1,15 @@
 package disenio;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.HashSet;
 import java.util.Set;
 
 //hoja del composite
 public class BajaDePoi implements Componente {
 	
+	private int id;
+	Terminal sistema=new Terminal();
 
 	public void agregarProceso(Componente unProceso){
 		
@@ -21,7 +25,58 @@ public class BajaDePoi implements Componente {
 	
 	public void ejecutar(){
 		
+		this.buscarPoi(id);
+		
 	}
 	
+	public Terminal getSistema(){
+		return sistema;
+	}
+
+
+	public void buscarPoi(int id){  
+		
+		if(!getSistema().getPois().isEmpty())
+			for (POI poi:getSistema().getPois()){
+				if(poi.getId()==id)
+				{
+					this.eliminarPOI(id);
+				}
+			}
+	}
+
+		
+
+
+		public Boolean eliminarPOI(int id){
+			
+			for(CGP cgp:getSistema().getCgps())
+			{
+				if(cgp.getId()==id)
+				{
+					getSistema().getCgps().remove(cgp);
+					getSistema().getConexion().update("jdbc:sqlserver://localhost;databaseName=bdpois;integratedSecurity=true","DELETE FROM Tabla_CGPs WHERE id="+id+";");
+				}
+			}
+			for(Banco banco:getSistema().getBancos())
+			{
+				if(banco.getId()==id)
+				{
+					getSistema().getCgps().remove(banco);
+					getSistema().getConexion().update("jdbc:sqlserver://localhost;databaseName=bdpois;integratedSecurity=true","DELETE FROM Tabla_Bancos WHERE id="+id+";");
+				}
+			}
+			for(POI poi:getSistema().getPois())
+			{
+				if(poi.getId()==id){
+					getSistema().getPois().remove(poi);
+					return true;
+				}
+			}
+			return false;
+		}
+		
+						
 	
 }
+	
