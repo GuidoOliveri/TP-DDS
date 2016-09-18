04b2a3e9f62b1533d1c61a1d0fcbc67e0636b823
GuidoOliveri <guido.oli@hotmail.com>
2016-09-17 21:57:24 -0300

    Loguin testeado con el ejemplo hardcodeado de pepe argento

--- DisenioDeSistemasUltimoo/src/disenio/Loguin.java
+++ DisenioDeSistemasUltimoo/src/disenio/Loguin.java
@@ -10,41 +10,52 @@ import javax.swing.JPanel;
 
 
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
+import java.awt.Font;
+import java.awt.Frame;
+
+import javax.swing.JPasswordField;
 
 public class Loguin extends JFrame {
-	private JTextField textField;
-	private JTextField textField_1;
+	public static JTextField txt_usuario;
 	public Loguin() {
+
+		JFrame frame = new JFrame();
+		frame.setBounds(100,100,485,407);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		setTitle("Evento de logueo");
 		getContentPane().setLayout(null);
 		
-		JLabel lblUsuario = new JLabel("Usuario");
-		lblUsuario.setBounds(67, 37, 46, 14);
+		JLabel lblUsuario = new JLabel("Usuario:");
+		lblUsuario.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblUsuario.setBounds(101, 37, 62, 14);
 		getContentPane().add(lblUsuario);
 		
-		textField = new JTextField();
-		textField.setBounds(132, 34, 86, 20);
-		getContentPane().add(textField);
-		textField.setColumns(10);
+		txt_usuario = new JTextField();
+		txt_usuario.setBounds(206, 36, 86, 20);
+		getContentPane().add(txt_usuario);
+		txt_usuario.setColumns(10);
 		
-		JLabel lblContrasea = new JLabel("Contrase\u00F1a");
-		lblContrasea.setBounds(32, 85, 81, 14);
+		JLabel lblContrasea = new JLabel("Contraseña:");
+		lblContrasea.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblContrasea.setBounds(75, 85, 103, 14);
 		getContentPane().add(lblContrasea);
 		
-		textField_1 = new JTextField();
-		textField_1.setBounds(132, 82, 86, 20);
-		getContentPane().add(textField_1);
-		textField_1.setColumns(10);
-		
 		JButton btnLoguearse = new JButton("Loguearse");
-		btnLoguearse.setBounds(45, 177, 89, 23);
+		btnLoguearse.setBounds(93, 177, 118, 42);
 		getContentPane().add(btnLoguearse);
 		EventoLog eventolog =new EventoLog();
 		btnLoguearse.addActionListener(eventolog);
 		
 		JButton btnCancelar = new JButton("Cancelar");
-		btnCancelar.setBounds(206, 177, 89, 23);
+		btnCancelar.setBounds(257, 177, 103, 42);
 		getContentPane().add(btnCancelar);
+		
+		pss_contraseña = new JPasswordField();
+		pss_contraseña.setBounds(206, 84, 86, 20);
+		getContentPane().add(pss_contraseña);
 		Cancelar canc = new Cancelar();
 		btnCancelar.addActionListener(canc);
 		
@@ -56,9 +67,15 @@ public class Loguin extends JFrame {
 	
 	}
 	
+	final DatosUsuario data = new DatosUsuario();
+	public static JPasswordField pss_contraseña;
 	private class EventoLog implements ActionListener{
 		public void actionPerformed(ActionEvent e) {
-			System.out.println("usuario logueado");
+			if(data.probarPass()==1){
+				JOptionPane.showMessageDialog(null, "Bienvenido al sistema de POIs Admin!");
+			}else{
+				JOptionPane.showMessageDialog(null, "ERROR, usuario o contraseña incorrectos");
+			}
 		}
 	}
 	
---
+++ DisenioDeSistemasUltimoo/src/disenio/PruebaLoguin.java
@@ -0,0 +1,8 @@
+package disenio;
+
+public class PruebaLoguin {
+	public static void main (String[] args){
+		Loguin milogueo = new Loguin();
+		milogueo.setVisible(true);
+	}
+}
